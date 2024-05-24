873c3c9c71856b0469b1e51cdb9b970fbd98f28b
Collins Omollo <omoloski5@gmail.com>
2024-05-24 10:13:24 +0300

    Doneeee

--- script.js
+++ script.js
@@ -110,5 +110,18 @@ $(document).ready(function() {
       });
     }
     
-  
-   
+// script.js
+dragElement(document.getElementById("navbar"));
+
+function dragElement(element) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(element.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(element.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    element.onmousedown = dragMouseDown;
+  }
+
+
+}
